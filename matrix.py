e851f717b51e2687b109ec92b4971bf431939b1c
nycz <pyntix@gmail.com>
2014-11-10 23:01:56 +0100

    First version of matrix.py, copied from cefyr/urd

---
+++ matrix.py
@@ -0,0 +1,93 @@
+def default_item():
+    return ('', (0,0))
+
+class Matrix():
+    def __init__(self, default_item, data=None):
+        self.default_item = default_item
+        if data:
+            self.data = data
+        else:
+            self.data = [[self.default_item()]]
+
+    def __str__(self):
+        return '\n'.join(repr(row) for row in self.data)
+        # return '\n'.join('\t|\t'.join(repr(x) for x in row) for row in self.data)
+
+    def __contains__(self, key):
+        if len(key) == 2:
+            x, y = key
+            return x in range(len(self.data[0])) and y in range(len(self.data))
+        else:
+            return False
+
+    def __getitem__(self, key):
+        x, y = key
+        return self.data[y][x]
+
+    def __setitem__(self, key, value):
+        x, y = key
+        if not value:
+            self.data[y][x] = default_item()
+            return
+        text, (w, h) = value
+        assert isinstance(text, str) and isinstance(w, int) and isinstance(h, int)
+        self.data[y][x] = value
+
+
+    def clear(self):
+        self.data = [[default_item()]]
+
+    def flip_orientation(self):
+        self.data = list(map(list, zip(*self.data)))
+
+    def count_rows(self):
+        return len(self.data)
+
+    def count_cols(self):
+        return len(self.data[0])
+
+    def add_row(self, pos=-1):
+        data = [default_item() for _ in range(len(self.data[0]))]
+        if pos == -1:
+            self.data.append(data)
+        else:
+            self.data.insert(pos, data)
+
+    def add_col(self, pos=-1):
+        for n in range(len(self.data)):
+            if pos == -1:
+                self.data[n].append(default_item())
+            else:
+                self.data[n].insert(pos, default_item())
+
+    def remove_row(self, pos):
+        del self.data[pos]
+
+    def remove_col(self, pos):
+        for n in range(len(self.data)):
+            del self.data[n][pos]
+
+    def move_row(self, oldpos, newpos):
+        row = self.data.pop(oldpos)
+        self.data.insert(newpos, row)
+
+    def move_col(self, oldpos, newpos):
+        for n in range(len(self.data)):
+            self.data[n].insert(newpos, oldpos)
+
+    def copy_row(self, oldpos, newpos):
+        row = self.data[oldpos].copy()
+        self.data.insert(newpos, row)
+
+    def copy_col(self, oldpos, newpos):
+        for n in range(len(self.data)):
+            if newpos == -1:
+                self.data[n].append(self.data[n][oldpos])
+            else:
+                self.data[n].insert(newpos, self.data[n][oldpos])
+
+    def row(self, pos):
+        return self.data[pos]
+
+    def col(self, pos):
+        return [x[pos] for x in self.data]
