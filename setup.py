d020cb61ab82dd3e520948c8ed1b7c1a45f5e76f
nycz <pyntix@gmail.com>
2021-01-24 13:50:40 +0100

    Set version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libsyntyche',
-    version='2.0',
+    version='2.0.0',
     author='nycz',
     packages=find_packages(),
     # install_requires=['PyQt5', 'PyYAML', 'pyenchant'],
