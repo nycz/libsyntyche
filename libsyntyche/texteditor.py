a6bb722d0a38ea01c9944d86f59a2704925d3f08
nycz <pyntix@gmail.com>
2021-01-30 16:09:51 +0100

    Rewrite the texteditor search/replace code
    Having it be a mixin was a pain so now it's just a regular object which
    take the Qt texteditor widget as an argument.

--- libsyntyche/texteditor.py
+++ libsyntyche/texteditor.py
@@ -1,163 +1,201 @@
+from dataclasses import dataclass, field
+import enum
 import re
-from typing import Callable, Optional
+from typing import Callable, Optional, Set, Union
 
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QTextCursor, QTextDocument
 
 
-class SearchAndReplaceable():
-    """
-    This implements search and replace functions for a QTextEdit,
-    QPlaintTextEdit, or another class that defines the following methods:
-        textCursor()
-        moveCursor(operation)
-        setTextCursor(cursor)
-        find(exp, options)
-    Since QTextEdit and QPlaintTextEdit don't have a common ancestor, this
-    class can't inherit from it. Instead: duck typing galore!
+# Flags
+_FLAG_BACKWARDS = 'b'
+_FLAG_CASE_INSENSITIVE = 'i'
+_FLAG_WHOLE_WORDS = 'w'
+_FLAG_COUNT = '#'
+_FLAG_REPLACE_ALL = 'a'
 
-    The command syntax is hardcoded.
-    """
-    def textCursor(self) -> QTextCursor:
-        raise NotImplementedError()
+_SEARCH_FLAGS = ''.join([
+    _FLAG_BACKWARDS,
+    _FLAG_CASE_INSENSITIVE,
+    _FLAG_WHOLE_WORDS,
+    _FLAG_COUNT,
+])
 
-    def moveCursor(self, operation: QTextCursor.MoveOperation) -> None:
-        raise NotImplementedError()
+_REPLACE_FLAGS = ''.join([
+    _FLAG_BACKWARDS,
+    _FLAG_CASE_INSENSITIVE,
+    _FLAG_WHOLE_WORDS,
+    _FLAG_REPLACE_ALL,
+])
 
-    def setTextCursor(self, cursor: QTextCursor) -> None:
-        raise NotImplementedError()
 
-    def find(self, exp: str, options: QTextDocument.FindFlags) -> bool:
-        raise NotImplementedError()
+# Regexes
+_SEARCH_PAYLOAD = r'(?:\\/|[^/])'
+_search_rx = re.compile(fr'{_SEARCH_PAYLOAD}+')
+_search_flags_rx = re.compile(fr"""
+    (?P<search>{_SEARCH_PAYLOAD}*?[^\\])
+    /
+    (?P<flags>[{_SEARCH_FLAGS}]*)
+""")
+_replace_rx = re.compile(fr"""
+    (?P<search>{_SEARCH_PAYLOAD}*?[^\\])
+    /
+    (?P<replace>({_SEARCH_PAYLOAD}*?[^\\])?)
+    /
+    (?P<flags>[{_REPLACE_FLAGS}]*)
+""", re.VERBOSE)
 
-    def initialize_search_and_replace(self, error: Callable[[str], None],
-                                      print_: Callable[[str], None]) -> None:
-        """
-        Initialize all the stuff needed for searching and replacing.
 
-        error and print_ are functions that take a string and output it in an
-        appropriate place, for example in a libsyntyche-style terminal.
+# State
+class SearchFlags(enum.Enum):
+    backwards = enum.auto()
+    case_insensitive = enum.auto()
+    whole_words = enum.auto()
 
-        As long as PyQt4 is used, multiple inheritence works kinda crappy.
-        For now, this method should be run instead of __init__ before any
-        other method in the class is used.
-        """
-        self.search_buffer: Optional[str] = None
+
+@dataclass
+class SearchState:
+    text: str
+    flags: Set[SearchFlags] = field(default_factory=set)
+
+    def find(self, editor: Union[QtWidgets.QPlainTextEdit, QtWidgets.QTextEdit],
+             override_flags: Optional[Set[SearchFlags]] = None) -> bool:
+        return editor.find(self.text,
+                           _encode_flags(override_flags if override_flags is not None
+                                         else self.flags))
+
+
+def _parse_flags(flag_str: str) -> Set[SearchFlags]:
+    out = set()
+    if _FLAG_BACKWARDS in flag_str:
+        out.add(SearchFlags.backwards)
+    if _FLAG_CASE_INSENSITIVE in flag_str:
+        out.add(SearchFlags.case_insensitive)
+    if _FLAG_WHOLE_WORDS in flag_str:
+        out.add(SearchFlags.whole_words)
+    return out
+
+
+def _encode_flags(flags: Set[SearchFlags]) -> QTextDocument.FindFlags:
+    search_flags = QTextDocument.FindFlags()
+    if SearchFlags.backwards in flags:
+        search_flags |= QTextDocument.FindBackward  # type: ignore
+    if SearchFlags.case_insensitive not in flags:
+        search_flags |= QTextDocument.FindCaseSensitively  # type: ignore
+    if SearchFlags.whole_words in flags:
+        search_flags |= QTextDocument.FindWholeWords  # type: ignore
+    return QTextDocument.FindFlags(search_flags)
+
+
+class Searcher:
+    def __init__(self, editor: Union[QtWidgets.QPlainTextEdit, QtWidgets.QTextEdit],
+                 error: Callable[[str], None],
+                 log: Callable[[str], None]) -> None:
+        self._editor = editor
+        self._search_state: Optional[SearchState] = None
         self.error = error
-        self.print_ = print_
+        self.log = log
 
-    def search_and_replace(self, arg: str) -> None:
+    def search_or_replace(self, cmd: str) -> None:
         """
-        Main search and replace function. This is most likely the method you
-        will want to use.
-
-        arg is a string with a vim-like s&r syntax (see the regexes below)
+        Search or replace, depending on how the cmd looks.
         """
-        def generate_flags(flagstr: str) -> None:
-            # self.search_flags is automatically generated and does not
-            # need to be initialized in __init__()
-            self.search_flags = QTextDocument.FindFlags()
-            if 'b' in flagstr:
-                self.search_flags |= QTextDocument.FindBackward
-            if 'i' not in flagstr:
-                self.search_flags |= QTextDocument.FindCaseSensitively
-            if 'w' in flagstr:
-                self.search_flags |= QTextDocument.FindWholeWords
-        search_rx = re.compile(r'([^/]|\\/)+$')
-        search_flags_rx = re.compile(r'([^/]|\\/)*?([^\\]/[biw]*)$')
-        replace_rx = re.compile(r"""
-            (?P<search>([^/]|\\/)*?[^\\])
-            /
-            (?P<replace>(([^/]|\\/)*[^\\])?)
-            /
-            (?P<flags>[abiw]*)
-            $
-        """, re.VERBOSE)
-        search_match = search_rx.match(arg)
-        search_flags_match = search_flags_rx.match(arg)
-        replace_match = replace_rx.match(arg)
-        if search_match:
-            self.search_buffer = search_match[0]
-            self.search_flags = QTextDocument.FindCaseSensitively
+        if (match := _search_rx.fullmatch(cmd)):
+            self._search_state = SearchState(match[0])
             self.search_next()
-        elif search_flags_match:
-            self.search_buffer, flags = search_flags_match[0]\
-                    .rsplit('/', 1)
-            generate_flags(flags)
-            self.search_next()
-        elif replace_match:
-            self.search_buffer = replace_match.group('search')
-            generate_flags(replace_match.group('flags'))
-            if 'a' in replace_match.group('flags'):
-                self._replace_all(replace_match.group('replace'))
+        elif (match := _search_flags_rx.fullmatch(cmd)):
+            search_buffer, flags = match['search'], match['flags']
+            if _FLAG_COUNT in flags:
+                self._count_hits(search_buffer, _parse_flags(flags))
             else:
-                self._replace_next(replace_match.group('replace'))
+                self._search_state = SearchState(search_buffer, _parse_flags(flags))
+                self.search_next()
+        elif (match := _replace_rx.fullmatch(cmd)):
+            self._search_state = SearchState(match['search'], _parse_flags(match['flags']))
+            if _FLAG_REPLACE_ALL in match['flags']:
+                self._replace_all(match['replace'])
+            else:
+                self._replace_next(match['replace'])
         else:
             self.error('Malformed search/replace expression')
 
-    def _searching_backwards(self) -> bool:
-        return bool(QTextDocument.FindBackward & self.search_flags)
-
-    def search_next(self) -> None:
-        """
-        Go to the next string found.
-
-        This does the same thing as running the same search-command again.
-        """
-        if self.search_buffer is None:
-            self.error('No previous searches')
-            return
-        temp_cursor = self.textCursor()
-        found = self.find(self.search_buffer, self.search_flags)
-        if not found:
-            if not self.textCursor().atStart() \
-                        or (self._searching_backwards()
-                            and not self.textCursor().atEnd()):
-                if self._searching_backwards():
-                    self.moveCursor(QTextCursor.End)
-                else:
-                    self.moveCursor(QTextCursor.Start)
-                found = self.find(self.search_buffer, self.search_flags)
-                if not found:
-                    self.setTextCursor(temp_cursor)
-                    self.error('Text not found')
-            else:
-                self.setTextCursor(temp_cursor)
-                self.error('Text not found')
+    @property
+    def _is_searching_backwards(self) -> bool:
+        return bool(self._search_state and SearchFlags.backwards in self._search_state.flags)
 
     def _replace_next(self, replace_buffer: str) -> None:
         """
         Go to the next string found and replace it with replace_buffer.
-
-        While this technically can be called from outside this class, it is
-        not recommended (and most likely needs some modifications of the code.)
         """
-        if self.search_buffer is None:
+        if self._search_state is None:
             self.error('No previous searches')
             return
-        temp_cursor = self.textCursor()
-        found = self.find(self.search_buffer, self.search_flags)
-        if not found:
-            if not self.textCursor().atStart() \
-                        or (self._searching_backwards()
-                            and not self.textCursor().atEnd()):
-                if self._searching_backwards():
-                    self.moveCursor(QTextCursor.End)
-                else:
-                    self.moveCursor(QTextCursor.Start)
-                found = self.find(self.search_buffer, self.search_flags)
-                if not found:
-                    self.setTextCursor(temp_cursor)
+        temp_cursor = self._editor.textCursor()
+        found = self._search_state.find(self._editor)
+        searching_backwards = self._is_searching_backwards
+        if not found and \
+                (not self._editor.textCursor().atStart()
+                 or (searching_backwards and not self._editor.textCursor().atEnd())):
+            self._editor.moveCursor(QTextCursor.End if searching_backwards
+                                    else QTextCursor.Start)
+            found = self._search_state.find(self._editor)
+            if not found:
+                self._editor.setTextCursor(temp_cursor)
         if found:
-            t = self.textCursor()
+            t = self._editor.textCursor()
             t.insertText(replace_buffer)
-            length = len(replace_buffer)
-            t.setPosition(t.position() - length)
-            t.setPosition(t.position() + length, QTextCursor.KeepAnchor)
-            self.setTextCursor(t)
-            self.print_(f'Replaced on line {t.blockNumber()}, '
-                        f'pos {t.positionInBlock()}')
+            replace_len = len(replace_buffer)
+            t.setPosition(t.position() - replace_len)
+            t.setPosition(t.position() + replace_len, QTextCursor.KeepAnchor)
+            self._editor.setTextCursor(t)
+            self.log(f'Replaced on line {t.blockNumber()}, pos {t.positionInBlock()}')
         else:
             self.error('Text not found')
+
+    def search_next(self, in_reverse: bool = False) -> None:
+        """
+        Go to the next string found.
+        """
+        if self._search_state is None:
+            self.error('No previous searches')
+            return
+        search_flags = self._search_state.flags.copy()
+        if in_reverse != self._is_searching_backwards:
+            searching_backwards = True
+            search_flags.add(SearchFlags.backwards)
+        else:
+            searching_backwards = False
+            search_flags.discard(SearchFlags.backwards)
+        temp_cursor = self._editor.textCursor()
+        found = self._search_state.find(self._editor, search_flags)
+        if not found and \
+                (not self._editor.textCursor().atStart()
+                 or (searching_backwards and not self._editor.textCursor().atEnd())):
+            self._editor.moveCursor(QTextCursor.End if searching_backwards
+                                    else QTextCursor.Start)
+            found = self._search_state.find(self._editor, search_flags)
+        if not found:
+            self._editor.setTextCursor(temp_cursor)
+            self.error('Text not found')
+
+    def _count_hits(self, target: str, flags: Set[SearchFlags]) -> None:
+        """
+        Count how many times a term (word/sentence, etc.) has been used.
+        """
+        times = 0
+        cursor = QTextCursor(self._editor.document())
+        # Don't use the backwards flag
+        flags = flags - {SearchFlags.backwards}
+        while True:
+            cursor = self._editor.document().find(target, cursor, _encode_flags(flags))
+            if not cursor.isNull():
+                times += 1
+            else:
+                break
+        if times:
+            self.log(f'The word "{target}" is used {times} times')
+        else:
+            self.log(f'The word "{target}" is not used')
 
     def _replace_all(self, replace_buffer: str) -> None:
         """
@@ -165,21 +203,20 @@ class SearchAndReplaceable():
 
         As with replace_next, you probably don't want to call this manually.
         """
-        if self.search_buffer is None:
-            self.error('No previous searches')
+        if self._search_state is None:
             return
-        temp_cursor = self.textCursor()
+        temp_cursor = self._editor.textCursor()
         times = 0
-        self.moveCursor(QTextCursor.Start)
+        self._editor.moveCursor(QTextCursor.Start)
         while True:
-            found = self.find(self.search_buffer, self.search_flags)
+            found = self._search_state.find(self._editor)
             if found:
-                self.textCursor().insertText(replace_buffer)
+                self._editor.textCursor().insertText(replace_buffer)
                 times += 1
             else:
                 break
         if times:
-            self.print_(f'{times} instance{"s" if times else ""} replaced')
+            self.log(f'{times} instance{"" if times == 1 else "s"} replaced')
         else:
             self.error('Text not found')
-        self.setTextCursor(temp_cursor)
+        self._editor.setTextCursor(temp_cursor)
